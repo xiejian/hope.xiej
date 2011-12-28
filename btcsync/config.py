e51086b5881bafd7436f1b9db9964979b0e7b0c7
unknown <xiej@xiej-PC.(none)>
2011-12-28 20:25:50 +0800

    home

--- btcsync/config.py
+++ btcsync/config.py
@@ -1,13 +1,13 @@
 
 database = {
-    'host': "192.168.131.7",
+    'host': "localhost",
 #    'port': 3306,
     'user': 'root',
     'passwd': 'xj',
     'db': 'btcfe',
 }
 
-btc_url = "http://u:p@192.168.168.122:8332/"
+btc_url = "http://u:p@10.8.0.6:8332/"
 btc_minconf = 2
 
 btc_syncinterv = 30
