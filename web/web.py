60c8bffa98616fa02e12229d4de914e0188e8c08
xiejian <xiejian.cn@gmail.com>
2011-12-29 17:36:53 +0800

    safe

--- web/web.py
+++ web/web.py
@@ -1,6 +1,6 @@
 import MySQLdb,user
 from contextlib import closing
-from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash
+from flask import Flask, request, session, redirect, url_for, abort,render_template, flash, g
 from basefunc import validateEmail,generate_csrf_token
 
 app = Flask(__name__)
@@ -36,8 +36,35 @@ def teardown_request(exception):
         g.cur.close()
     if hasattr(g, 'db'):
         g.db.close()
+def init_site():
+    pass
 
 #=================================================================
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    error = None
+    if request.method == 'POST':
+        if not validateEmail(request.form['username']):
+            error = 'Not validate Email'
+        #elif len(request.form['password']) < 6:
+        #    error = 'Password too Short'
+        else:
+            user_id = user.loginuser(request.form['username'],request.form['password'])
+            if user_id:
+                session['logged_in'] = True
+                user.update_gu(user_id)
+                flash('You were logged in')
+                return redirect(url_for('trade'))
+            else:
+                error = 'Login Failed.'
+    return render_template('home.html',error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -74,46 +101,21 @@ def trade():
     if request.method == 'POST':
         pass
     else:
-        g.cur.execute("SELECT contract_id,NAME,btc_multi,opendate,settledate,leverage FROM contract WHERE STATUS = 'O'")
-        contracts = [dict(contract_id=row[0],name=row[1],btc_multi=row[2],opendate=row[3],settledate=row[4],leverage=row[5]) for row in g.cur.fetchall()]
-        return render_template('trade.html',contracts=contracts)
+        return render_template('trade.html')
 
 @app.route('/account', methods=['GET','POST'])
 def account():
-    g.cur.execute('SELECT contract_id,contract_name,buy_sell,lots,cost,marketvalue,margin FROM v_pos WHERE user_id=%s', session['user_id'])
-    pos = [dict(contract_id=row[0],contract_name=row[1],buy_sell=row[2],lots=row[3],cost=row[4],marketvalue=row[5],margin=row[6]) for row in g.cur.fetchall()]
-    return render_template('account.html',pos = pos)
+    return render_template('account.html')
 
 
-@app.route('/', methods=['GET','POST'])
-def home():
-    error = None
-    if request.method == 'POST':
-        if not validateEmail(request.form['username']):
-            error = 'Not validate Email'
-        #elif len(request.form['password']) < 6:
-        #    error = 'Password too Short'
-        else:
-            user_id = user.loginuser(request.form['username'],request.form['password'])
-            if user_id:
-                session['logged_in'] = True
-                session['username'] = request.form['username']
-                session['user_id'] = user_id
-                flash('You were logged in')
-                return redirect(url_for('trade'))
-            else:
-                error = 'Login Failed.'
-    return render_template('home.html',error = error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('home'))
+@app.route('/test', methods=['GET','POST'])
+def test():
+    user.update_gu(2)
+    return session['email']
 
 
 if __name__ == '__main__':
+    init_site()
     app.run()
 
 
